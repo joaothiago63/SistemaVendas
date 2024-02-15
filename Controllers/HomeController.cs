673ee354fe2946963b349a9d746d1adfd36b11a2
João Thiago <contato_joaothiago@hotmail.com>
2024-02-14 21:23:54 -0300

    Conectando Banco

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Models;
+using SistemaVendas.Uteis;
+using System.Diagnostics;
+
+namespace SistemaVendas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DAL obj = new DAL();
+            obj.ExecutarComandoSql("INSERT INTO VENDEDOR (nome, email, senha) values ('João','contato_joaothiago@hotmail.com','123456')");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Uteis/DAL.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SistemaVendas.Uteis
+{
+    public class DAL
+    {
+        private static string Server = "localhost";
+        private static string DataBase = "sistema_venda";
+        private static string User = "root";
+        private static string Password = "";
+        private static string ConnectionString = $"Server={Server};Database={DataBase};Uid={User};Pwd={Password};Sslmode=none;";
+        private static MySqlConnection Connection;
+
+        public DAL()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+
+        }
+ 
+        /*ESSE METODO RECEBE UMA STRING COMO COMANDO EM SQL PARA SELECT*/
+        public DataTable RetDataTable(string sql)
+        {
+            DataTable data = new DataTable();
+            MySqlCommand Comand = new MySqlCommand(sql, Connection);
+            MySqlDataAdapter da = new MySqlDataAdapter(Comand);
+
+            da.Fill(data);
+            return data;
+        }
+
+        /*ESPERA UM PARAMETRO DO TIPO STRING
+         CONTENDO UM COMANDO SQL DO TIPO INSERT, UPDATE, DELETE*/
+        public void ExecutarComandoSql(string sql)
+        {
+            MySqlCommand Comand = new MySqlCommand(sql, Connection);
+            Comand.ExecuteNonQuery();
+        }
+
+    }
+
+}
+
